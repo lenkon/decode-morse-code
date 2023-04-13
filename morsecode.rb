775427fb1b23dba8390ea1d5484c34b6c1de67ec
lenkon <inoulenkon@gmail.com>
2023-04-13 20:38:32 +0530

    Add functions to decode word and full message

--- morsecode.rb
+++ morsecode.rb
@@ -10,3 +10,22 @@ def decode_letter(encrypted_char)
   }
   code_char_map[encrypted_char]
 end
+
+def decode_word(encrypted_word)
+  plain_text = ''
+  encrypted_word.split.each do |encrypted_char|
+    plain_text += decode_letter(encrypted_char)
+  end
+  plain_text
+end
+
+def decode(encrypted_text)
+  decrypted_text = []
+
+  encrypted_text.split('   ').each do |encrypted_word|
+    decrypted_text.push(decode_word(encrypted_word))
+  end
+  decrypted_text.join(' ')
+end
+
+print decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
